628dcce49f16b1df5e26fbb778a78a80318528e3
Azmi Daşman <azmidasman@gmail.com>
2022-04-14 14:35:38 +0300

    Add project files.

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;//Şayet bu modeli tanıtmazsam çalışmaz.
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities1 db=new MvcDbStokEntities1(); //Tablolara ulaşmak için db nesnesi türetiyorum.
+        //Bu benim modelimi tutuyor. Modelimde de tablolarım var.
+        public ActionResult Index()
+        {
+            var degerler=db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View(); //Sayfa çağrıldıgında sadece view getirsin. yoksa her sayfa açıldıgında kategori ekler.
+            //Thats why we use httpget and httppost. these are different things..
+        }
+
+
+        [HttpPost] //Butona tıkladıgımda çalışacak.
+        public ActionResult YeniKategori(TBLKATEGORILER p1) //Kategori ekleme için böyle bir şey yaptım. parametre1
+        {
+            db.TBLKATEGORILER.Add(p1); //Gelen parametreyi tbl kategorilere ekle
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult SIL(int id) //Silme işlemi için id değişkeni tanımladım. ID'ye göre sildireceğim.
+        {
+            var kategori=db.TBLKATEGORILER.Find(id); //TBLKAtegorilerde bul neyi bul id'den gelen değeri bul.
+            db.TBLKATEGORILER.Remove(kategori);//TBLKAtegorilerdeki değeri sil neyi sil kategori değişkeninden geleni.
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GUNCELLE(int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir",ktgr);
+        }
+    }
+}
---
+++ MvcStok/Controllers/UrunController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;//İlk olarak using yapıyoruz. Projedeki Modeldeki Entitiyi kullan
+
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        //Ardından db üretiyoruz ki moıdele erişebillim.
+        MvcDbStokEntities1 db = new MvcDbStokEntities1();
+        public ActionResult Index() //Son olarak da index'e view ekliyoruz.
+        {
+            //Ardından ürünleri listede tutacak değişkeni tanımlamak lazım.
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler); //Tabi bu değişkeni viewda return ettirmemiz lazım..
+        }
+
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            //Aşağıdaki liste kategorileri sıralı getirmek için. linq sorgusu var. Dropdown'u araştır.
+            //SelectListItem Sadewce orada kullanılıyor.
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text=i.KATEGORIAD,
+                                                 Value=i.KATEGORIID.ToString(),
+                                             }).ToList();
+            ViewBag.dgr=degerler; //Controller'dan diğer tarafa almak için view'ü çantaya koymak lazım.
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniUrun(TBLURUNLER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault(); //Dropdown list için.
+            p1.TBLKATEGORILER = ktg;
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SIL(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
